5afc8ee3c0d144827cb845db3e3d90d0b7de4bfe
Phil B <phil@berryman.org.uk>
2018-09-21 12:52:30 +0100

    added function to return film when clicked on - still no styling

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 let favourites = JSON.parse(localStorage.getItem('favourites'));
 // console.log(favourites);
 
-const container = document.querySelector('.app');
-container.addEventListener('click', event => {
-    if (event.target.matches('.movies__fav-icon')) {
-        toggleFavourite(event.target.id, event.target.parentElement.id);
-    }
-    if (event.target.parentNode.matches('.result')) {
-        showMovie(event.target.parentNode.id)
-    }
-});
+// creates all event listeners that are needed
+const createListeners = () => {
+    const container = document.querySelector('.app');
+    container.addEventListener('click', event => {
+        if (event.target.matches('.movies__fav-icon')) {
+            toggleFavourite(event.target.id, event.target.parentElement.id);
+        }
+        if (event.target.parentNode.matches('.result')) {
+            showMovie(event.target.parentNode.id)
+        }
+    });
+}
 
+// builds the url for omdb api request
 const createUrl = (typeOfSearch, search) => {
     const baseURL = "http://www.omdbapi.com/";
     const apiKey = "95869d44";
     return `${baseURL}?apikey=${apiKey}&${typeOfSearch}=${search}`
 }
 
+// makes all requests to omdb api
+const apiRequest = (typeOfSearch = "s", search) => {
+    return fetch(createUrl(typeOfSearch, search))
+        .then(function (response) {
+            return response.json();
+        })
+}
 
-const apiRequest = (typeOfSearch="s", search) => {
-    return fetch(createUrl(typeOfSearch,search))
-            .then(function (response) {
-                return response.json();
-            })
-        }
+// SEARCH RESULTS FUNCTIONS :: 
+// These functions get search results from API and render them
 
+// inserts html into .search div - not Pure (altering dom)
+const renderMovieResults = resultsHtml => {
+    const resultsDiv = document.querySelector('.results');
+    resultsDiv.innerHTML = resultsHtml;
+}
 
 // template for search results
 const searchTemplate = (result) => {
@@ -36,34 +48,46 @@ const searchTemplate = (result) => {
     </div>`
 };
 
-// inserts html into .search div - not Pure (altering dom)
-const renderMovieResults = resultsHtml => {
-    const resultsDiv = document.querySelector('.results');
-    resultsDiv.innerHTML = resultsHtml;
-}
 
 // takes search query / uses apiRequest to fetch results. Maps through and returns html
 const createSearchHtml = (search) => {
-    return apiRequest('s',search).then(function (body){
+    return apiRequest('s', search).then(function (body) {
         const apiResults = body.Search;
         const resultsHtml = apiResults.map((result) => {
-        return searchTemplate(result)
+            return searchTemplate(result)
         }).join("");
         renderMovieResults(resultsHtml);
     });
 }
 
-const createMovieHtml = (id) => {
-    return apiRequest('t',id).then(function (body){
-        console.log(body);
+// MOVIE FUNCTIONS :: 
+//These 3 functions get a movie from the API and render it.
+const showMovie = (id) => {
+    return apiRequest('t', id).then(function (body) {
+        renderMovie(movieTemplate(body));
     });
 }
 
-const showMovie = (id) => {
-    createMovieHtml(id);
+const movieTemplate = (movie) => {
+    return `
+    <div class="movie__title">${movie.Title}</div>
+    <div class="movie__title">${movie.Year}</div>
+    <div class="movie__title">${movie.Poster}</div>
+    <div class="movie__title">${movie.Title}</div>
+    <div class="movie__title">${movie.Year}</div>
+    <div class="movie__title">${movie.Poster}</div>
+    <div class="movie__title">${movie.Title}</div>
+    <div class="movie__title">${movie.Year}</div>
+    <div class="movie__title">${movie.Poster}</div>
+    `
+}
+const renderMovie = movieHtml => {
+    const movieDiv = document.querySelector('.movie');
+    movieDiv.innerHTML = movieHtml;
 }
 
-const toggleFavourite = (filmID,filmTitle) => {
+
+const toggleFavourite = (filmID, filmTitle) => {
     console.log(123)
     // console.log(filmName);
     let filmA = '#' + filmID;
@@ -77,11 +101,12 @@ const toggleFavourite = (filmID,filmTitle) => {
         console.log("toggle " + filmTitle);
         updatedFavourites[filmTitle] = 1;
     } else {
-        updatedFavourites[filmTitle] = 0;  
+        updatedFavourites[filmTitle] = 0;
     }
     // updatedFavourites.filmName = 
     localStorage.setItem('favourites', JSON.stringify(updatedFavourites));
     console.log(updatedFavourites);
 }
 
+createListeners();
 createSearchHtml('pulp')
